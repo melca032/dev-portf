2142cfcab03a58ab0922c4f55ea039f9223b9a1b
melca032 <122252713+melca032@users.noreply.github.com>
2023-01-09 14:48:05 +0800

    update

---
+++ base-template/js/data/projects-data.js
@@ -0,0 +1,65 @@
+/*=================================================================
+  The Project and Project Modal Elements just share the same data
+=================================================================*/
+
+const trafficLight = {
+    name: "Traffic Light",
+    label: "Embedded System",
+    description:
+      "Three alternative methods were used to create this traffic light: the 555 Timer IC, Arduino, and STM32.",
+    logo: "../../images/projects/logo/trafficlight (2).png",
+    demoImage: "../../images/projects/demo/trafficlight (2).png",
+    demoImageAlt: "Traffic Light",
+    demoLink: "https://github.com/BrianMara2000/",
+    sourceCodeLink: "https://github.com/BrianMara2000/",
+    tags: ["555 Timer IC", "Arduino", "STM32"],
+  };
+  
+  const soapDispenser = {
+    name: "Automatic Soap Dispenser",
+    label: "Microcontroller",
+    description:
+      "Creating a Automatic Soap Dispenser.",
+    logo: "../../images/projects/logo/soap1.jpg",
+    demoImage: "../../images/projects/demo/soap1.jpg",
+    demoImageAlt: "Automatic Soap Dispenser",
+    demoLink: "https://github.com/BrianMara2000/",
+    sourceCodeLink: "https://github.com/BrianMara2000/",
+    tags: ["Arduino"],
+  };
+  
+  const calculator = {
+    name: "Calculator",
+    label: "Logic Circuit",
+    description:
+      "A calculator is a device that performs arithmetic operations on numbers. Basic calculators can do only addition, subtraction, multiplication and division mathematical calculations.",
+    logo: "../../images/projects/logo/2dc1.png",
+    demoImage: "../../images/projects/demo/2dc1.png",
+    demoImageAlt: "Logic Circuit",
+    demoLink: "https://github.com/BrianMara2000/",
+    sourceCodeLink: "https://github.com/BrianMara2000/",
+    tags: ["Logic Circuit"],
+  };
+  
+  const lfr = {
+    name: "Line Follower Robot",
+    label: "Line Follower Robot with PID using STM32",
+    description:
+      "This repository contains the implementation of an optimized PID controller for a line follower robot using STM32F103C8 microcontroller and BFD-1000 reflectance sensor array.",
+    logo: "../../images/projects/logo/linefollowerrobot.jpg",
+    demoImage: "../../images/projects/demo/linefollowerrobot.jpg",
+    demoImageAlt: "Line Follower Robot with PID using STM32",
+    demoLink: "https://github.com/BrianMara2000/",
+    sourceCodeLink: "https://github.com/BrianMara2000/",
+    tags: ["stm", "C"],
+  };
+  
+
+  
+  export const projects = [
+    trafficLight,
+    soapDispenser,
+    calculator,
+    lfr,
+  ];
+  
---
+++ base-template/js/data/skills-data.js
@@ -0,0 +1,66 @@
+const html = {
+    icon: "../../images/icons/skills/html.png",
+    label: "HTML",
+  };
+  
+  const css = {
+    icon: "../../images/icons/skills/css.png",
+    label: "CSS",
+  }; 
+  const wpf = {
+    icon: "../../images/icons/skills/wpf.png",
+    label: "WPF",
+  };
+  
+  const winforms = {
+    icon: "../../images/icons/skills/winforms.png",
+    label: "WinForms",
+  };
+  
+  const csharp = {
+    icon: "../../images/icons/skills/csharp.svg",
+    label: "C#",
+  };
+  
+  const sqlServer = {
+    icon: "../../images/icons/skills/ms-sql-server.png",
+    label: "Microsoft Sql Server",
+  };
+  const java = {
+    icon: "../../images/icons/skills/java.png",
+    label: "Java",
+  };
+  
+  const laravel = {
+    icon: "../../images/icons/skills/laravel.png",
+    label: "Laravel",
+  };
+
+  const c = {
+    icon: "../../images/icons/skills/c.png",
+    label: "C",
+  };
+
+  const arduino = {
+    icon: "../../images/icons/skills/arduino.png",
+    label: "Arduino",
+  };
+ 
+  const stm32 = {
+    icon: "../../images/icons/skills/STM32.png",
+    label: "STM32",
+  };
+  export const skills = [
+    csharp,
+    wpf,
+    winforms,
+    sqlServer,
+    html,
+    css,
+    java,
+    laravel,
+    c,
+    arduino,
+    stm32,
+  ];
+  
